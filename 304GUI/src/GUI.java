452d226feee2f272d806c69772abaa1a0545ea60
Scott Luu <p2d9@lin19.ugrad.cs.ubc.ca>
2017-03-15 18:43:37 -0700

    Finished layout for customer panel

--- 304GUI/src/GUI.java
+++ 304GUI/src/GUI.java
@@ -11,6 +11,10 @@ import javax.swing.ImageIcon;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.DocumentEvent;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
@@ -38,7 +42,16 @@ public class GUI extends JPanel implements ActionListener {
             public void actionPerformed(ActionEvent e) {
                 JFrame frame = new JFrame ("Customer Panel");
                 //frame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+                
                 frame.getContentPane().add (new CustPanel());
+                frame.addComponentListener(new ComponentAdapter() {
+                	public void componentResized(ComponentEvent e) {
+                		CustPanel temp = (CustPanel) frame.getContentPane().getComponent(0);
+                		temp.width = temp.getWidth();
+                		temp.height = temp.getHeight();
+                		temp.setPositions();
+                	}
+                });
                 frame.pack();
                 frame.setVisible(true);
             }
@@ -176,40 +189,144 @@ class CustPanel extends JPanel {
     JLabel l1, l2;
     JButton b1;
     JTextField t1, t2;
-    JCheckBox cb1;
+    JCheckBox cb1, cb2;
+    JRadioButton rb1, rb2;
+    ButtonGroup bg1;
+    public int width;
+    public int height;
 
+    public void updateResults() {
+    	System.out.println("updating");
+    }
+
+    public void setPositions() {
+    	l1.setBounds(width/2-100,  5,         100, 25);
+        l2.setBounds(width/2,      5,         100, 25);
+        t1.setBounds(width/2-100,  35,        100, 25);
+        t2.setBounds(width/2,      35,        100, 25);
+        rb1.setBounds(width/2-100, 65,        50,  25);
+        rb2.setBounds(width/2-50,  65,        100, 25);
+        cb1.setBounds(5,           height-60, 100,  25);
+        cb2.setBounds(5,           height-30, 100, 25);
+    }
 
     public CustPanel () {
+
         l1 = new JLabel("Price");
         l2 = new JLabel("Name");
-        b1 = new JButton("Go");
+        //b1 = new JButton("Go");
         t1 = new JTextField(10);
         t2 = new JTextField(10);
         cb1= new JCheckBox("Price");
-
-
-
-        setPreferredSize (new Dimension (395, 156));
-        b1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String text1 = t1.getText();
-                String text2 = t2.getText();
-                System.out.println(text1 + text2);
-                //jcomp4.selectAll();
-            }
+        cb2= new JCheckBox("Name");
+        rb1 = new JRadioButton("<=");
+        rb2 = new JRadioButton(">");
+        bg1 = new ButtonGroup();
+        bg1.add(rb1);
+        bg1.add(rb2);
+        width = 400;
+        height = 400;
+        rb1.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		updateResults();
+        	}
         });
-        t1.setBounds (205, 115, 100, 25);
-        t2.setBounds (205, 115, 100, 25);
+        rb2.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		updateResults();
+        	}
+        });
+        cb1.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		updateResults();
+        	}
+        });
+        cb2.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		updateResults();
+        	}
+        });
+        t1.getDocument().addDocumentListener(new DocumentListener() {
+        	public void changedUpdate(DocumentEvent e) {
+        		if (isValid()) {
+        			update();
+        		}
+        	}
+        	public void removeUpdate(DocumentEvent e) {
+        		if (isValid()) {
+        			update();
+        		}
+        	}
+        	public void insertUpdate(DocumentEvent e) {
+        		if (isValid()) {
+        			update();
+        		}
+        	}
+        	public boolean isValid() {
+        		if (t1.getText().equals("")) {
+        			return true;
+        		}
+        		int value;
+        		try {
+        			value = Integer.parseInt(t1.getText()); 
+        		} catch (NumberFormatException e) {
+        			JOptionPane.showMessageDialog(null,
+        				"Please enter a number",
+        				"Error",
+        				JOptionPane.ERROR_MESSAGE);
+        			return false;
+        		}
+        		if (value < 0) {
+        			JOptionPane.showMessageDialog(null,
+        				"Please enter a number greater than 0",
+        				"Error",
+        				JOptionPane.ERROR_MESSAGE);
+        			return false;
+        		} else {
+        			return true;
+        		}
+        	}
+        	public void update() {
+        		updateResults();
+        	}
+        });
+        t2.getDocument().addDocumentListener(new DocumentListener() {
+        	public void changedUpdate(DocumentEvent e) {
+        		updateResults();
+        	}
+        	public void removeUpdate(DocumentEvent e) {
+        		updateResults();
+        	}
+        	public void insertUpdate(DocumentEvent e) {
+        		updateResults();
+       		}
+        });
+        setPreferredSize (new Dimension (width, height));
+        // b1.addActionListener(new ActionListener() {
+        //     @Override
+        //     public void actionPerformed(ActionEvent e) {
+        //         String text1 = t1.getText();
+        //         String text2 = t2.getText();
+        //         System.out.println(text1 + text2);
+        //         //jcomp4.selectAll();
+        //     }
+        // });
+        
+        //b1.setBounds(width-100,    height-30, 100, 25);
+        setPositions();
         //b1.setVerticalTextPosition(AbstractButton.BOTTOM);
         //b1.setHorizontalTextPosition(AbstractButton.RIGHT);
         //b1.setBounds(300,120,10,10);
+        setLayout(null);
         add(l1);
         add(l2);
-        add(b1);
         add(t1);
         add(t2);
+        add(rb1);
+        add(rb2);
         add(cb1);
+        add(cb2);
+        //add(b1);
     }
 
 }
